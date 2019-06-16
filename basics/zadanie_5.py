f32f7b9966330617d210227e7137508fe64fbf43
Rafał <korzeniewski@gmail.com>
2019-06-16 09:48:43 +0200

    Fix w zadaniu 5

--- basics/zadanie_5.py
+++ basics/zadanie_5.py
@@ -1,13 +1,13 @@
 
 miasto_a = input("Miasto A: ")
 miasto_b = input("Miasto B: ")
-# dystans = float(input("Dystans" + miasto_a + "-" + miasto_b + ": "))
+
 dystans = float(input(f"Dystans {miasto_a}-{miasto_b }: "))
 cena = float(input("Cena paliwa: "))
 spalanie = float(input("Spalanie na 100 km: "))
 print()
 
-koszt = (dystans / 100) * spalanie *  cena)
+koszt = (dystans / 100) * spalanie *  cena
 koszt = round(koszt, 0)
 
 print("Koszt przejazdu",miasto_a + "-" + miasto_b , koszt ,"PLN")
